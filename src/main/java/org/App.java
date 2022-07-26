f469367dc3824fd0cfc3cad8946a4a0551896c53
valard <kolya666kv@gmail.com>
2022-07-26 15:03:37 +0300

    maven: - updated deps - updated props java: - server boilerplate added

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -1,10 +1,20 @@
 package org;
 
+import io.undertow.Undertow;
+import io.undertow.util.Headers;
+
 public class App {
 
     public static void main(String ...args) {
 
-
+        Undertow.builder()
+                .addHttpListener(8000, "localhost")
+                .setHandler(ex -> {
+                    ex.getResponseHeaders().add(Headers.CONTENT_TYPE, "application/json");
+                    ex.getResponseSender().send("{\"msg\":\"application alive\"}");
+                })
+                .build()
+                .start();
 
     }
 
